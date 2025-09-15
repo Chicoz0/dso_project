6b47d09a7cd34e998546c3630ab9bd9a866b8402
marloncorreia <malilocsimas26@gmail.com>
2025-09-15 16:30:03 -0300

    feat: base attraction model

---
+++ models/event/attraction.py
@@ -0,0 +1,43 @@
+from utils.id import generate_id
+
+
+class Attraction:
+    def __init__(self, id: int, name: str, attraction_type: str):
+        if not isinstance(id, int) or not id:
+            raise Exception("Invalid id")
+
+        if not isinstance(name, str) or not name:
+            raise Exception("Invalid name")
+
+        if not isinstance(attraction_type, str) or not attraction_type:
+            raise Exception("Invalid Attraction Type")
+
+        self.__id = generate_id()
+        self.__name = name
+        self.__attraction_type = attraction_type
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str):
+        if not isinstance(new_name, str) or not new_name:
+            raise Exception("Invalid name")
+
+        self.__name = new_name
+
+    @property
+    def attraction_type(self):
+        return self.__attraction_type
+
+    @attraction_type.setter
+    def attraction_type(self, new_attraction_type: str):
+        if not isinstance(new_attraction_type, str) or not new_attraction_type:
+            raise Exception("Invalid attraction_type")
+
+        self.__attraction_type = new_attraction_type

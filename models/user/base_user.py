bbcce3d72cea73cc6bcba02adc0d5f502e5d41f5
Francisco de Paula Lemos <franciscolemosfisio@gmail.com>
2025-09-15 16:52:06 -0300

    feat: improved BaseUser class

--- models/user/base_user.py
+++ models/user/base_user.py
@@ -1,44 +1,41 @@
 from abc import ABC, abstractmethod
+from utils.id import generate_id
 
 class BaseUser(ABC):
     @abstractmethod
-    def __init__(self, id: int, login: str, password_token: str, email: str):
-        self.__id = None
-        self.__login = ""
-        self.__password_token = ""
-        self.__email = ""
-
-        if isinstance(id, int) and id is not None:
-            self.__id = id
-        if isinstance(login, str) and login is not None:
-            self.__login = login
-        if isinstance(password_token, str) and password_token is not None:
-            self.__password_token = password_token
-        if isinstance(email, str) and email is not None:
-            self.__email = email
+    def __init__(self, login, password, email):
+        if not isinstance(login, str) or login is None:
+            raise ValueError("login must be a string and not null")        
+        if not isinstance(password, str) or password is None:
+            raise ValueError("password must be a string and not null")
+        if not isinstance(email, str) or email is None:
+            raise ValueError("email must be a string and not null")
+        self.__id = generate_id()
+        self.__password = password
+        self.__login = login
+        self.__email = email
 
     @property
     def id(self):
-        return self._id
+        return self.__id
 
     @property
     def login(self):
-        return self._login
-
-    @property
-    def password(self):
-        return self._password_token
+        return self.__login
 
     @property
     def email(self):
-        return self._email
+        return self.__email
 
-    def change_password(self, new_password_token: str):
-        if isinstance(new_password_token, str) and new_password_token is not None:
-            self.__password_token = new_password_token
-            print(f"Password for {self.__login} has been changed.")
+    # Most likely we will use bcrypt in the future to handle passwords
+    def change_password(self, new_password: str):
+        if not isinstance(new_password, str) or new_password is None:
+            raise ValueError("New password must be a string and not null")
+        self.__password = new_password
+        print(f"Password for {self.__login} altered.")
 
     def change_email(self, new_email: str):
-        if isinstance(new_email, str) and new_email is not None:
-            self.__email = new_email
-            print(f"Email for {self.__login} has been changed to {self.__email}.")
+        if not isinstance(new_email, str) or new_email is None:
+            raise ValueError("New email must be a string and not null")
+        self.__email = new_email
+        print(f"Email for {self.__login} altered to {self.__email}.")

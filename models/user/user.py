8b0c307122128568caf78f10548d8c1bd416d7b2
Francisco de Paula Lemos <franciscolemosfisio@gmail.com>
2025-09-15 17:47:18 -0300

    feat: base User model

--- models/user/user.py
+++ models/user/user.py
@@ -1,4 +1,11 @@
-class User:
-    def __init__(self):
-        # ToDo: Implement here
-        pass
+from base_user import BaseUser
+from user_status import UserStatus
+
+class User(BaseUser):
+    def __init__(self, login: str, password: str, email: str):
+        super().__init__(login, password, email)   
+        self.__status = UserStatus.ACTIVE
+
+    @property
+    def status(self):
+        return self.__status
